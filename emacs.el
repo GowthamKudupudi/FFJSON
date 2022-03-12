99b2b4d35ce7dc2a0e57bafb38a823ed52e3ae0c
Necktwi Ozfguah <Necktwi@Blizlac.FerryFair.com>
2022-03-12 17:20:55 +0530

    emacs.el

---
+++ emacs.el
@@ -0,0 +1,22 @@
+;;; Package --- Summary
+;;; Commentary:
+;;; It sets cmake-ide variables
+
+;;; Code:
+(defvar CMakeProject "FFJSON")
+(require 'cmake-ide)
+
+(setq cmake-ide-build-dir
+      (concat cmake-ide-project-dir "build/Linux/x86-64/debug/"))
+(setq cmake-ide-build-pool-dir cmake-ide-build-dir)
+(make-directory cmake-ide-build-dir t)
+(setq cmake-ide-cmake-opts
+      (concat "-D_DEBUG=1 -DCMAKE_BUILD_TYPE=Debug "
+	           "-DCMAKE_EXPORT_COMPILE_COMMANDS=1"
+              "-DBUILD_TESTING=1"))
+(defvar MakeThreadCount 1)
+(defvar DebugTarget "TestFFJSON")
+(defvar TargetArguments "-s normal")
+(message "FFJSON emacs project loaded.")
+(provide 'FFJSON)
+;;; emacs.el ends here
